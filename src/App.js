44e50ef7677388205bd12e8c6e6310df8d1cd0e5
kabilansakthivelu <kabilansakthi97@gmail.com>
2021-08-08 19:50:50 +0530

    Fix for scrolling issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,99 @@
 import Header from "./Components/Header";
 import Teaser from "./Components/Teaser";
 import Cards from "./Components/Cards";
+import { useFetch } from "../src/Components/useFetch";
+
+const netflixOriginalsURL =
+  "https://api.themoviedb.org/3/discover/tv?api_key=896c8566fc255f7c52f6ea6bd2901188&with_networks=213";
+const trendingNowURL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=896c8566fc255f7c52f6ea6bd2901188&with_genres=10749";
+const topRatedURL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=896c8566fc255f7c52f6ea6bd2901188&with_genres=27";
+const actionMoviesURL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=896c8566fc255f7c52f6ea6bd2901188&with_genres=35";
+const comedyMoviesURL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=896c8566fc255f7c52f6ea6bd2901188&with_genres=28";
+const horrorMoviesURL =
+  "https://api.themoviedb.org/3/movie/top_rated?api_key=896c8566fc255f7c52f6ea6bd2901188&language=en-US";
+const romanceMoviesURL =
+  "https://api.themoviedb.org/3/trending/all/week?api_key=896c8566fc255f7c52f6ea6bd2901188&language=en-US";
+const documentariesURL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=896c8566fc255f7c52f6ea6bd2901188&with_genres=99";
 
 function App() {
+  const netflixOriginalsCards = useFetch(netflixOriginalsURL);
+  const trendingNowCards = useFetch(trendingNowURL);
+  const topRatedCards = useFetch(topRatedURL);
+  const actionMoviesCards = useFetch(actionMoviesURL);
+  const comedyMoviesCards = useFetch(comedyMoviesURL);
+  const horrorMoviesCards = useFetch(horrorMoviesURL);
+  const romanceMoviesCards = useFetch(romanceMoviesURL);
+  const documentariesCards = useFetch(documentariesURL);
+
   return (
     <div className="App">
       <Header />
+
       {/* <Teaser /> */}
-      <Cards />
+
+      <div className="contentCards">
+        {/* NETFLIX ORIGINALS */}
+        <Cards
+          title="NETFLIX ORIGINALS"
+          classNames="section1"
+          id="one"
+          cards={netflixOriginalsCards}
+        />
+        {/* Trending Now */}
+        <Cards
+          title="Trending Now"
+          classNames="section2"
+          id="two"
+          cards={trendingNowCards}
+        />
+        {/* Top Rated */}
+        <Cards
+          title="Top Rated"
+          classNames="section3"
+          id="three"
+          cards={topRatedCards}
+        />
+        {/* Action Movies */}
+        <Cards
+          title="Action Movies"
+          classNames="section4"
+          id="four"
+          cards={actionMoviesCards}
+        />
+        {/* Comedy Movies */}
+        <Cards
+          title="Comedy Movies"
+          classNames="section5"
+          id="five"
+          cards={comedyMoviesCards}
+        />
+        {/* Horror Movies */}
+        <Cards
+          title="Horror Movies"
+          classNames="section6"
+          id="six"
+          cards={horrorMoviesCards}
+        />
+        {/* Romance Movies */}
+        <Cards
+          title="Romance Movies"
+          classNames="section7"
+          id="seven"
+          cards={romanceMoviesCards}
+        />
+        {/* Documentaries */}
+        <Cards
+          title="Documentaries"
+          classNames="section8"
+          id="eight"
+          cards={documentariesCards}
+        />
+      </div>
     </div>
   );
 }
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,254 +1,53 @@
 import React, { useState } from "react";
-import { useFetch } from "./useFetch";
 import { BiLeftArrowCircle, BiRightArrowCircle } from "react-icons/bi";
 
-const netflixOriginalsURL =
-  "https://api.themoviedb.org/3/discover/tv?api_key=896c8566fc255f7c52f6ea6bd2901188&with_networks=213";
-const trendingNowURL =
-  "https://api.themoviedb.org/3/discover/movie?api_key=896c8566fc255f7c52f6ea6bd2901188&with_genres=10749";
-const topRatedURL =
-  "https://api.themoviedb.org/3/discover/movie?api_key=896c8566fc255f7c52f6ea6bd2901188&with_genres=27";
-const actionMoviesURL =
-  "https://api.themoviedb.org/3/discover/movie?api_key=896c8566fc255f7c52f6ea6bd2901188&with_genres=35";
-const comedyMoviesURL =
-  "https://api.themoviedb.org/3/discover/movie?api_key=896c8566fc255f7c52f6ea6bd2901188&with_genres=28";
-const horrorMoviesURL =
-  "https://api.themoviedb.org/3/movie/top_rated?api_key=896c8566fc255f7c52f6ea6bd2901188&language=en-US";
-const romanceMoviesURL =
-  "https://api.themoviedb.org/3/trending/all/week?api_key=896c8566fc255f7c52f6ea6bd2901188&language=en-US";
-const documentariesURL =
-  "https://api.themoviedb.org/3/discover/movie?api_key=896c8566fc255f7c52f6ea6bd2901188&with_genres=99";
-
-const Test = () => {
-  const netflixOriginalsCards = useFetch(netflixOriginalsURL);
-  const trendingNowCards = useFetch(trendingNowURL);
-  const topRatedCards = useFetch(topRatedURL);
-  const actionMoviesCards = useFetch(actionMoviesURL);
-  const comedyMoviesCards = useFetch(comedyMoviesURL);
-  const horrorMoviesCards = useFetch(horrorMoviesURL);
-  const romanceMoviesCards = useFetch(romanceMoviesURL);
-  const documentariesCards = useFetch(documentariesURL);
-
-  const [sectionOneArrows, setSectionOneArrows] = useState("hide");
-  const [sectionTwoArrows, setSectionTwoArrows] = useState("hide");
-  const [sectionThreeArrows, setSectionThreeArrows] = useState("hide");
-  const [sectionFourArrows, setSectionFourArrows] = useState("hide");
-  const [sectionFiveArrows, setSectionFiveArrows] = useState("hide");
-  const [sectionSixArrows, setSectionSixArrows] = useState("hide");
-  const [sectionSevenArrows, setSectionSevenArrows] = useState("hide");
-  const [sectionEightArrows, setSectionEightArrows] = useState("hide");
+const Cards = ({ title, classNames, id, cards }) => {
+  const [arrows, setArrows] = useState("hide");
 
   return (
     <article className="bground">
       <div className="cards">
-        {/* Netflix originals section */}
-
-        <h1 className="cards-heading">NETFLIX ORIGINALS</h1>
+        <h1 className="cards-heading">{title}</h1>
         <div
-          id="one"
-          className="individualSection"
+          className={classNames}
           onMouseEnter={() => {
-            setSectionOneArrows("show");
+            setArrows("show");
           }}
           onMouseLeave={() => {
-            setSectionOneArrows("hide");
+            setArrows("hide");
           }}
         >
-          {sectionOneArrows === "show" ? (
-            <BiLeftArrowCircle className="leftArrow" />
-          ) : null}
-          {netflixOriginalsCards.map((item) => {
-            return (
-              <img src={item.poster} alt="" className="image" key={item.id} />
-            );
-          })}
-          {sectionOneArrows === "show" ? (
-            <BiRightArrowCircle
-              className="rightArrow"
-              onClick={() => {
-                document.getElementById("one").scrollLeft += 1000;
-              }}
-            />
-          ) : null}
-        </div>
-
-        {/* trending Now section */}
-
-        <h1 className="cards-heading">Trending Now</h1>
-        <div
-          className="individualSection"
-          onMouseEnter={() => {
-            setSectionTwoArrows("show");
-          }}
-          onMouseLeave={() => {
-            setSectionTwoArrows("hide");
-          }}
-        >
-          {sectionTwoArrows === "show" ? (
-            <BiLeftArrowCircle className="leftArrow" />
-          ) : null}
-          {trendingNowCards.map((item) => {
-            return (
-              <img src={item.poster} alt="" className="image" key={item.id} />
-            );
-          })}
-          {sectionTwoArrows === "show" ? (
-            <BiRightArrowCircle className="rightArrow" />
-          ) : null}
-        </div>
-
-        {/* Top Rated section */}
-
-        <h1 className="cards-heading">Top Rated</h1>
-        <div
-          className="individualSection"
-          onMouseEnter={() => {
-            setSectionThreeArrows("show");
-          }}
-          onMouseLeave={() => {
-            setSectionThreeArrows("hide");
-          }}
-        >
-          {sectionThreeArrows === "show" ? (
-            <BiLeftArrowCircle className="leftArrow" />
-          ) : null}
-          {topRatedCards.map((item) => {
-            return (
-              <img src={item.poster} alt="" className="image" key={item.id} />
-            );
-          })}
-          {sectionThreeArrows === "show" ? (
-            <BiRightArrowCircle className="rightArrow" />
-          ) : null}
-        </div>
-
-        {/* Action movies section */}
-
-        <h1 className="cards-heading">Action Movies</h1>
-        <div
-          className="individualSection"
-          onMouseEnter={() => {
-            setSectionFourArrows("show");
-          }}
-          onMouseLeave={() => {
-            setSectionFourArrows("hide");
-          }}
-        >
-          {sectionFourArrows === "show" ? (
-            <BiLeftArrowCircle className="leftArrow" />
-          ) : null}
-          {actionMoviesCards.map((item) => {
-            return (
-              <img src={item.poster} alt="" className="image" key={item.id} />
-            );
-          })}
-          {sectionFourArrows === "show" ? (
-            <BiRightArrowCircle className="rightArrow" />
-          ) : null}
-        </div>
-
-        {/* Comedy Movies section */}
-
-        <h1 className="cards-heading">Comedy Movies</h1>
-        <div
-          className="individualSection"
-          onMouseEnter={() => {
-            setSectionFiveArrows("show");
-          }}
-          onMouseLeave={() => {
-            setSectionFiveArrows("hide");
-          }}
-        >
-          {sectionFiveArrows === "show" ? (
-            <BiLeftArrowCircle className="leftArrow" />
-          ) : null}
-          {comedyMoviesCards.map((item) => {
-            return (
-              <img src={item.poster} alt="" className="image" key={item.id} />
-            );
-          })}
-          {sectionFiveArrows === "show" ? (
-            <BiRightArrowCircle className="rightArrow" />
-          ) : null}
-        </div>
-
-        {/* Horror Movies section */}
-
-        <h1 className="cards-heading">Horror Movies</h1>
-        <div
-          className="individualSection"
-          onMouseEnter={() => {
-            setSectionSixArrows("show");
-          }}
-          onMouseLeave={() => {
-            setSectionSixArrows("hide");
-          }}
-        >
-          {sectionSixArrows === "show" ? (
-            <BiLeftArrowCircle className="leftArrow" />
-          ) : null}
-          {horrorMoviesCards.map((item) => {
-            return (
-              <img src={item.poster} alt="" className="image" key={item.id} />
-            );
-          })}
-          {sectionSixArrows === "show" ? (
-            <BiRightArrowCircle className="rightArrow" />
-          ) : null}
-        </div>
-
-        {/* Romance Movies section */}
-
-        <h1 className="cards-heading">Romance Movies</h1>
-        <div
-          className="individualSection"
-          onMouseEnter={() => {
-            setSectionSevenArrows("show");
-          }}
-          onMouseLeave={() => {
-            setSectionSevenArrows("hide");
-          }}
-        >
-          {sectionSevenArrows === "show" ? (
-            <BiLeftArrowCircle className="leftArrow" />
-          ) : null}
-          {romanceMoviesCards.map((item) => {
-            return (
-              <img src={item.poster} alt="" className="image" key={item.id} />
-            );
-          })}
-          {sectionSevenArrows === "show" ? (
-            <BiRightArrowCircle className="rightArrow" />
-          ) : null}
-        </div>
-
-        {/* Dcumentaries section */}
-
-        <h1 className="cards-heading">Documentaries</h1>
-        <div
-          className="individualSection"
-          onMouseEnter={() => {
-            setSectionEightArrows("show");
-          }}
-          onMouseLeave={() => {
-            setSectionEightArrows("hide");
-          }}
-        >
-          {sectionEightArrows === "show" ? (
-            <BiLeftArrowCircle className="leftArrow" />
-          ) : null}
-          {documentariesCards.map((item) => {
-            return (
-              <img src={item.poster} alt="" className="image" key={item.id} />
-            );
-          })}
-          {sectionEightArrows === "show" ? (
-            <BiRightArrowCircle className="rightArrow" />
-          ) : null}
+          <div className="left">
+            {arrows === "show" ? (
+              <BiLeftArrowCircle
+                className="leftArrow"
+                onClick={() => {
+                  document.getElementById(id).scrollLeft -= 1200;
+                }}
+              />
+            ) : null}
+          </div>
+          <div className="individualSection" id={id}>
+            {cards.map((item) => {
+              return (
+                <img src={item.poster} alt="" className="image" key={item.id} />
+              );
+            })}
+          </div>
+          <div className="right">
+            {arrows === "show" ? (
+              <BiRightArrowCircle
+                className="rightArrow"
+                onClick={() => {
+                  document.getElementById(id).scrollLeft += 1200;
+                }}
+              />
+            ) : null}
+          </div>
         </div>
       </div>
     </article>
   );
 };
 
-export default Test;
+export default Cards;
